6a16b942bf4f5e08ad0d9e2a4bff273af54defbf
kzmiura <136795515+kzmiura@users.noreply.github.com>
2025-12-03 10:49:08 +0900

    Implemented.

---
+++ src/lib.rs
@@ -0,0 +1,139 @@
+use io::prelude::*;
+use io::{BufReader, BufWriter};
+use std::io;
+use std::net::TcpStream;
+
+struct RconPacket {
+    id: i32,
+    typ: i32,
+    body: String,
+}
+
+impl RconPacket {
+    const SERVERDATA_AUTH: i32 = 3;
+    const SERVERDATA_AUTH_RESPONSE: i32 = 2;
+    const SERVERDATA_EXECCOMMAND: i32 = 2;
+    const SERVERDATA_RESPONSE_VALUE: i32 = 0;
+}
+
+pub struct RconClient {
+    buf_reader: BufReader<TcpStream>,
+    buf_writer: BufWriter<TcpStream>,
+}
+
+impl RconClient {
+    pub fn new(stream: &TcpStream) -> io::Result<Self> {
+        let buf_reader = BufReader::new(stream.try_clone()?);
+        let buf_writer = BufWriter::new(stream.try_clone()?);
+        Ok(RconClient {
+            buf_reader,
+            buf_writer,
+        })
+    }
+
+    fn read_rcon_from(&mut self) -> io::Result<RconPacket> {
+        let mut buf = [0; 4];
+        self.buf_reader.read_exact(&mut buf)?;
+        let size = i32::from_le_bytes(buf);
+
+        let mut buf = vec![0; size.try_into().unwrap()];
+        self.buf_reader.read_exact(&mut buf)?;
+        let (id_bytes, remainder) = buf
+            .split_first_chunk_mut::<4>()
+            .expect("Rcon packet should have an id");
+        let (typ_bytes, remainder) = remainder
+            .split_first_chunk_mut::<4>()
+            .expect("Rcon packet should have a type");
+        let (body_bytes, _null_bytes) = remainder
+            .split_last_chunk::<2>()
+            .expect("Rcon packet should have a body and two null bytes");
+
+        let id = i32::from_le_bytes(*id_bytes);
+        let typ = i32::from_le_bytes(*typ_bytes);
+        let body = str::from_utf8(body_bytes).expect("Rcon packet body should be valid ASCII");
+
+        Ok(RconPacket {
+            id,
+            typ,
+            body: body.into(),
+        })
+    }
+
+    fn write_rcon_to(&mut self, packet: RconPacket) -> io::Result<()> {
+        let RconPacket { id, typ, body } = packet;
+
+        let body_bytes = body.into_bytes();
+        let size = i32::try_from(2 * size_of::<i32>() + body_bytes.len() + 2)
+            .expect("Rcon packet size should fit in i32");
+        for bytes in [
+            &size.to_le_bytes(),
+            &id.to_le_bytes(),
+            &typ.to_le_bytes(),
+            &body_bytes[..],
+            &[0; 2],
+        ] {
+            self.buf_writer.write_all(bytes)?;
+        }
+        self.buf_writer.flush()?;
+
+        Ok(())
+    }
+
+    pub fn authenticate(&mut self, password: &str) -> io::Result<bool> {
+        let packet = RconPacket {
+            id: 1,
+            typ: RconPacket::SERVERDATA_AUTH,
+            body: password.into(),
+        };
+        self.write_rcon_to(packet)?;
+        match self.read_rcon_from()? {
+            RconPacket {
+                id: 1,
+                typ: RconPacket::SERVERDATA_AUTH_RESPONSE,
+                body: _,
+            } => Ok(true),
+            RconPacket {
+                id: -1,
+                typ: RconPacket::SERVERDATA_AUTH_RESPONSE,
+                body: _,
+            } => Ok(false),
+            _ => panic!("Unexpected Rcon packet during authentication"),
+        }
+    }
+
+    pub fn execute_command(&mut self, command: &str) -> io::Result<String> {
+        let packet = RconPacket {
+            id: 1,
+            typ: RconPacket::SERVERDATA_EXECCOMMAND,
+            body: command.into(),
+        };
+        self.write_rcon_to(packet)?;
+        let marker = RconPacket {
+            id: -1,
+            typ: RconPacket::SERVERDATA_RESPONSE_VALUE,
+            body: String::new(),
+        };
+        self.write_rcon_to(marker)?;
+
+        let mut result = String::new();
+        loop {
+            match self.read_rcon_from()? {
+                RconPacket {
+                    id: -1,
+                    typ: RconPacket::SERVERDATA_RESPONSE_VALUE,
+                    body: _,
+                } => {
+                    break Ok(result);
+                }
+                RconPacket {
+                    id: 1,
+                    typ: RconPacket::SERVERDATA_RESPONSE_VALUE,
+                    body,
+                } => {
+                    result += &body;
+                }
+                _ => panic!("Unexpected Rcon packet during command execution"),
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use io::IsTerminal;
+use io::prelude::*;
+use std::io;
+use std::net::TcpStream;
+use std::process::ExitCode;
+
+use rscon::RconClient;
+
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+struct Cli {
+    host: String,
+    port: u16,
+    #[arg(short, long)]
+    password: String,
+}
+
+fn main() -> ExitCode {
+    if let Err(e) = run(Cli::parse()) {
+        eprintln!("{e}");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
+    }
+}
+
+fn run(cli: Cli) -> io::Result<()> {
+    let Cli {
+        host,
+        port,
+        password,
+    } = cli;
+    let stream = TcpStream::connect((host, port))?;
+    let mut client = RconClient::new(&stream)?;
+    if !client.authenticate(&password)? {
+        eprintln!("Authentication failed");
+    } else {
+        let mut buf = String::new();
+        loop {
+            buf.clear();
+            if io::stdout().is_terminal() {
+                print!(">>> ");
+                io::stdout().flush()?;
+            }
+            if io::stdin().read_line(&mut buf)? == 0 {
+                break;
+            }
+
+            let command = buf.trim_end();
+            let resp = client.execute_command(command)?;
+            println!("{}", resp);
+        }
+    }
+
+    Ok(())
 }
